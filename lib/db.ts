3ca35a10b1dd5b7e2308fcbff86c27fea48cf13e
Rey-Gidds <reygidwani2006@gmail.com>
2026-04-01 19:59:25 +0530

    Add: Database MongoDB connect function to connect to the database URI as the app starts.

---
+++ lib/db.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+  var mongoose: any;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+    if (!MONGODB_URI) {
+        throw new Error("Please add your Mongo URI to .env.local");
+    }
+    if (cached.conn) {
+        return cached.conn;
+    }
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI!);
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+export default connectDB;
+
