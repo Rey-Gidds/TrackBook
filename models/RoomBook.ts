409b9370cd9f122409b90822487b45e57ac98bb8
Rey-Gidds <reygidwani2006@gmail.com>
2026-04-25 10:51:22 +0530

    Add: Room book, each room holds its one expense collectionm (book)

---
+++ models/RoomBook.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document, model, models } from "mongoose";
+
+export interface IRoomBook extends Document {
+  roomId: mongoose.Types.ObjectId;
+  tickets: mongoose.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const RoomBookSchema = new Schema<IRoomBook>(
+  {
+    roomId: { type: Schema.Types.ObjectId, ref: "Room", required: true },
+    tickets: [{ type: Schema.Types.ObjectId, ref: "RoomTicket" }],
+  },
+  { timestamps: true }
+);
+
+const RoomBook = models.RoomBook || model<IRoomBook>("RoomBook", RoomBookSchema);
+export default RoomBook;
